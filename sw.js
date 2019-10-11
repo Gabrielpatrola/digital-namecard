0bf7545d095adf194ad653d066fc91ac0e559f1a
Gabriel Augusto "Patrola" Almeida <almeidagabrielaugusto@gmail.com>
2019-10-11 16:52:03 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-/** Um service worker que não faz nada! */
-self.addEventListener('fetch', function(event) {
-  /** Um listener que não faz nada! */
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/main.css',
+  '/js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
